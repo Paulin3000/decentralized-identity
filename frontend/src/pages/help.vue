0648391aa135e9a63b04dacd821f5f5d1acb119e
Paulin3000 <116294797+Paulin3000@users.noreply.github.com>
2025-04-17 14:57:32 +0200

    updated data in credentialStore.js

--- frontend/src/pages/help.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <Header title="Help Section"> </Header>
-  </div>
-</template>
-<script setup lang="ts">
-import Header from "../components/Header.vue";
-</script>
--- frontend/src/pages/settings.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <Header title="Settings"> </Header>
-  </div>
-</template>
-<script setup lang="ts">
-import Header from "../components/Header.vue";
-</script>
